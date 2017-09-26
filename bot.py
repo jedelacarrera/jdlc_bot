f96199d2053dbe4790e8c6c911ba8ac3feac102f
jedelacarrera <jedelacarrera@uc.cl>
2017-09-25 23:37:22 -0300

    new error

--- bot.py
+++ bot.py
@@ -103,8 +103,8 @@ class Player(telepot.aio.helper.ChatHandler):
 		return
 
 
-	async def on__idle(self, event):
-		self.close()
+	# async def on__idle(self, event):
+	# 	self.close()
 
 
 TOKEN = "332206016:AAGW4YCE24LL-cUrXsBZ83BshyUK9ejpnOs"
